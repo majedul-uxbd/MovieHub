cb630b6f32ed80a06e38b441c8f2d50150d6a82c
majedul-uxbd <majedul@uxd.co.jp>
2025-04-28 14:02:45 +0600

    init project

--- interfaces/session.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface UserSession {
-    id: string,
-    user_id: string,
-    firstName: string,
-    lastName: string,
-    position: string,
-    role: string,
-    profile_img: string | null,
-}
